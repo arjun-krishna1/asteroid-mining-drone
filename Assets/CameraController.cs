3b0ec3a1e1402d6c23047aa536fa0122c957ce19
arjunkrishna <arjun.krishna@huawei.com>
2021-06-12 14:13:07 -0400

    add script to take picture of drone camera output and save + rotate drone

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     [Range(0.0f, 45.0f)]
-    public float rotationSpeed;
-    private string basePath = "~/Desktop/sphere-generate-random/";
+    public float rotationSpeed = 0.5f;
+    private string basePath = System.IO.Directory.GetCurrentDirectory()+"/data/" ;
+
     void Update()
     {
-        // ScreenCapture.CaptureScreenshot(basePath + "frame"+Time.frameCount+".png");
-        // print(transform.eulerAngles.ToString());
-        transform.Rotate(0, rotationSpeed, 0.0f, Space.Self);
+        ScreenCapture.CaptureScreenshot(basePath + "frame"+Time.frameCount+"-"+transform.eulerAngles.ToString()+".png");
+        transform.Rotate(rotationSpeed, rotationSpeed, 0.0f, Space.Self);
     }
 }
